d160b70a9c309386a65cacc0c527a878f713d1eb
Andrew Glago <andrewglago1@gmail.com>
2023-05-21 14:20:40 +0000

    feat: add components entry point

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'tsup';
 
 export default defineConfig({
-  entry: ['src/index.ts'],
+  entry: ['src/index.ts', 'src/components/index.ts'],
   treeshake: true,
   sourcemap: 'inline',
   minify: true,
