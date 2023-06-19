a81434c58b9c6154d9f79d2a4c21e3274ef379f5
Andrew Glago <andrewglago1@gmail.com>
2023-06-19 17:07:22 +0000

    feat: add renderer component

--- vue-sdk/src/index.ts
+++ vue-sdk/src/index.ts
@@ -3,3 +3,7 @@ export { plugin as pccPlugin } from "./plugin";
 export type { PantheonClientConfig } from "./core/pantheon-client";
 
 export * from "./hooks";
+
+export * from "./types";
+
+export * from "./components";
