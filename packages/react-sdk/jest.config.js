9f65789ecfd32e6ee67d7b2d3e1788db7553df0c
Andrew Glago <andrewglago1@gmail.com>
2023-08-01 16:13:24 +0000

    chore: add linting for react sdk

--- packages/react-sdk/jest.config.js
+++ packages/react-sdk/jest.config.js
@@ -1,5 +1,14 @@
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
+
+const esmModules = ["react-markdown"];
+
 module.exports = {
   preset: "ts-jest",
   testEnvironment: "node",
+  transform: {
+    "^.+\\.tsx?$": "ts-jest",
+  },
+  transformIgnorePatterns: [
+    `<rootDir>/node_modules/(?!(?:.pnpm/)?(${esmModules.join("|")})@)`,
+  ],
 };
