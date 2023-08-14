d855d8e06ff3f27689c9ba57c2dbcc16dc96f680
Andrew Glago <andrewglago1@gmail.com>
2023-08-14 14:58:20 +0000

    fix(starters): null check updatedAt

--- starters/nextjs-starter/pages/articles/[...uri].jsx
+++ starters/nextjs-starter/pages/articles/[...uri].jsx
@@ -19,14 +19,16 @@ export default function PageTemplate({ article }) {
             <div>
               <h1 className="text-3xl font-bold md:text-4xl">{titleElement}</h1>
 
-              <p className="py-2">
-                Last Updated:{" "}
-                {new Date(article.updatedAt).toLocaleDateString("en-US", {
-                  year: "numeric",
-                  month: "long",
-                  day: "numeric",
-                })}
-              </p>
+              {article.updatedAt ? (
+                <p className="py-2">
+                  Last Updated:{" "}
+                  {new Date(article.updatedAt).toLocaleDateString("en-US", {
+                    year: "numeric",
+                    month: "long",
+                    day: "numeric",
+                  })}
+                </p>
+              ) : null}
 
               <hr className="mt-6 mb-8" />
             </div>
