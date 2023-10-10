673172e86b02561f83d33d22f56b7869fe02b683
Andrew Glago <andrewglago1@gmail.com>
2023-10-10 13:26:50 +0000

    fix(vue-sdk): remove vue-markdown dep, fix vue 3 import error

--- packages/vue-sdk/src/components/Renderer/index.ts
+++ packages/vue-sdk/src/components/Renderer/index.ts
@@ -1,6 +1,8 @@
 import { Article } from "@pantheon-systems/pcc-sdk-core/types";
-import { defineComponent, h, PropType } from "vue-demi";
-import VueMarkdown from "vue-markdown-render";
+import type { Options as MarkdownItOptions } from "markdown-it";
+// @ts-expect-error - no types
+import MarkdownIt from "markdown-it/dist/markdown-it.js";
+import { computed, defineComponent, h, PropType } from "vue-demi";
 import TopLevelElement from "./TopLevelElement";
 
 export type JSONElement = {
@@ -16,6 +18,35 @@ export type JSONElement = {
   attrs?: Record<string, string>;
 };
 
+const MarkdownRenderer = defineComponent({
+  name: "VueMarkdown",
+  props: {
+    source: {
+      type: String,
+      required: true,
+    },
+    options: {
+      type: Object as PropType<MarkdownItOptions>,
+      default: () => ({}),
+      required: false,
+    },
+  },
+  setup(props, { attrs }) {
+    const md = new MarkdownIt(props.options);
+
+    const content = computed(() => {
+      const src = props.source;
+      return md?.render(src);
+    });
+
+    return () =>
+      h("div", {
+        ...attrs,
+        innerHTML: content.value,
+      });
+  },
+});
+
 const Renderer = defineComponent({
   name: "Renderer",
   props: {
@@ -29,8 +60,8 @@ const Renderer = defineComponent({
     return () =>
       h("div", {}, [
         article.contentType === "TEXT_MARKDOWN"
-          ? h(VueMarkdown, {
-              source: article.content,
+          ? h(MarkdownRenderer, {
+              source: article.content || "",
               options: {
                 html: true,
               },
--- packages/vue-sdk/src/index.ts
+++ packages/vue-sdk/src/index.ts
@@ -10,5 +10,3 @@ export * from "@pantheon-systems/pcc-sdk-core";
 export { plugin as pccPlugin } from "./plugin";
 
 export * from "./hooks";
-
-export * from "./components";
