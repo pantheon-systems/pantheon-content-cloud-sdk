c16aa4fca96efd1669fb3dc2e5ddf332ef591f7a
Andrew Glago <andrewglago1@gmail.com>
2023-10-10 10:04:54 +0000

    feat: setup vue ts starter

---
+++ starters/vue-starter-ts/app.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import Header from './components/Header.vue'
+
+</script>
+
+<template>
+  <Header/>
+  <div>
+    <NuxtWelcome />
+  </div>
+</template>
---
+++ starters/vue-starter-ts/components/Header.vue
@@ -0,0 +1,8 @@
+<script setup>
+</script>
+
+<template>
+  <header>
+    <h1 class="text-red-400">Vue 3 + TypeScript</h1>
+  </header>
+</template>
