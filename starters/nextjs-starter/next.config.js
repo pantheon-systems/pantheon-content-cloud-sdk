7889000adbdcd70e1ebf5b180abcd09c24a2065e
Omkar <aumkardange@gmail.com>
2023-10-19 16:03:36 +0530

    Removed warning when running nextjs

--- starters/nextjs-starter/next.config.js
+++ starters/nextjs-starter/next.config.js
@@ -10,11 +10,11 @@ function ensureEnvVariable(name) {
   if (process.env[name] == null) {
     if (process.env.NODE_ENV === "development") {
       throw new Error(
-        `No ${name} found.\nSee the README.md for information on setting this variable locally.`
+        `No ${name} found.\nSee the README.md for information on setting this variable locally.`,
       );
     } else if (process.env.NODE_ENV === "production") {
       throw new Error(
-        `No ${name} Endpoint found.\nLink to your PCC Instance or set the ${name} environment variable in the settings tab in the dashboard\nIf your site does not require a backend to build, remove this check from the next.config.js.`
+        `No ${name} Endpoint found.\nLink to your PCC Instance or set the ${name} environment variable in the settings tab in the dashboard\nIf your site does not require a backend to build, remove this check from the next.config.js.`,
       );
     }
   }
@@ -29,9 +29,9 @@ if (process.env.IS_CICD !== "true") {
 const nextConfig = {
   reactStrictMode: true,
   env: {
-    PCC_HOST: process.env.PCC_HOST,
     PCC_SITE_ID: process.env.PCC_SITE_ID,
     PCC_API_KEY: process.env.PCC_API_KEY,
+    ...(process.env.PCC_HOST && { PCC_HOST: process.env.PCC_HOST }),
   },
   output: "standalone",
 };
