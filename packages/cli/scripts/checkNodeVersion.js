3cf720c80aab6054d9f63c03ec745d80ba23d010
Omkar <aumkardange@gmail.com>
2023-10-26 14:34:36 +0530

    fixed script to check node js version

--- packages/cli/scripts/checkNodeVersion.js
+++ packages/cli/scripts/checkNodeVersion.js
@@ -1,11 +1,9 @@
-const result = process.versions.node;
+const nodeVersion = process.versions.node;
 const checkNodeVersion = (version) => {
-  const versionRegex = new RegExp(`^${version}\\..*`);
-  const versionCorrect = result.match(versionRegex);
-  if (!versionCorrect) {
+  if (parseInt(nodeVersion) < 16) {
     process.stdout.write(
       "\x1b[31m" +
-        `ERROR: Running on older Node.js version("${result}"). Please upgrade the Node.js version to "${version}" or higher.\n`,
+        `ERROR: Running on older Node.js version("${nodeVersion}"). Please upgrade the Node.js version to "${version}" or higher.\n`,
     );
     process.exit(1);
   }
