e6ea374f24164d8cec591defee8b76231c9f9a14
Omkar <aumkardange@gmail.com>
2023-11-02 21:07:00 +0530

    added script to check npm package updates

--- packages/cli/src/lib/checkUpdate.ts
+++ packages/cli/src/lib/checkUpdate.ts
@@ -1,42 +1,27 @@
-import * as fs from "fs";
+import fs from "fs";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import boxen from "boxen";
 import chalk from "chalk";
 import pkgJson from "package-json";
 import semver from "semver";
-import semverDiff from "semver-diff";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-function capitalizeFirstLetter(string) {
-  return string.charAt(0).toUpperCase() + string.slice(1);
-}
-
 const checkUpdate = async () => {
   const { name, version } = JSON.parse(
-    fs.readFileSync(__dirname + "/../package.json"),
+    fs.readFileSync(__dirname + "/../package.json").toString(),
   );
   const { version: latestVersion } = await pkgJson(name);
-
   // check if local package version is less than the remote version
-  const updateAvailable = semver.lt(version, latestVersion);
-
+  const updateAvailable = semver.lt(version, latestVersion as string);
   if (updateAvailable) {
-    let updateType = "";
-
     // check the type of version difference which is usually patch, minor, major etc.
-    let verDiff = semverDiff(version, latestVersion);
-
-    if (verDiff) {
-      updateType = capitalizeFirstLetter(verDiff);
-    }
-
     const msg = {
-      updateAvailable: `${updateType} update available ${chalk.dim(
-        version,
-      )} → ${chalk.green(latestVersion)}`,
+      updateAvailable: `Update available! ${chalk.dim(version)} → ${chalk.green(
+        latestVersion,
+      )}`,
       runUpdate: `Run ${chalk.cyan(`npm i -g ${name}`)} to update`,
     };
 
@@ -50,4 +35,5 @@ const checkUpdate = async () => {
     );
   }
 };
-checkUpdate();
+
+export default checkUpdate;
