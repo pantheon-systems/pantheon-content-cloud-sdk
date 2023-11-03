ad781a0cc7ed4114f2fc661a5e14835fc23eb40e
Andrew Glago <andrewglago1@gmail.com>
2023-11-03 14:48:59 +0000

    feat: download only required starter kit

---
+++ packages/cli/src/lib/downloadTemplateDirectory.ts
@@ -0,0 +1,80 @@
+import { mkdir, writeFile } from "fs/promises";
+import path from "path";
+import axios from "axios";
+import { Octokit } from "octokit";
+
+const octokit = new Octokit();
+
+const owner = "pantheon-systems";
+const repo = "pantheon-content-cloud-sdk";
+
+interface File {
+  path: string;
+  contents: Buffer;
+}
+
+interface TreeNode {
+  path: string;
+}
+
+export async function downloadTemplateDirectory(
+  directory: string,
+  outputDirectory: string,
+  printVerbose?: boolean,
+) {
+  try {
+    const files = await fetchFiles(directory, printVerbose);
+
+    await Promise.all(files.map((file) => output(file, outputDirectory)));
+
+    return path.resolve(outputDirectory);
+  } catch (error) {
+    throw new Error("Error fetching starter kit from Github: " + error);
+  }
+}
+
+async function output(file: File, outputDirectory: string) {
+  const outputPath = path.join(outputDirectory, file.path);
+  const dir = path.dirname(outputPath);
+
+  await mkdir(dir, { recursive: true });
+  await writeFile(outputPath, file.contents);
+}
+
+async function fetchFiles(directory: string, printVerbose?: boolean) {
+  const { data } = await octokit.request(
+    "GET /repos/{owner}/{repo}/git/trees/{tree_sha}",
+    {
+      owner,
+      repo,
+      tree_sha: "main",
+      recursive: "true",
+    },
+  );
+
+  const files = data.tree
+    .filter((node): node is TreeNode =>
+      Boolean(node.path?.startsWith(directory) && node.type === "blob"),
+    )
+    .map((node) => node.path);
+
+  const downloadPromises = files.map(async (path) => {
+    if (printVerbose) {
+      console.log(`Downloading ${path}`);
+    }
+
+    const { data } = await axios.get(
+      `https://raw.githubusercontent.com/${owner}/${repo}/main/${path}`,
+      {
+        responseType: "text",
+      },
+    );
+
+    return {
+      path: path.replace(directory, ""),
+      contents: Buffer.from(data, "utf-8"),
+    };
+  });
+
+  return Promise.all(downloadPromises);
+}
