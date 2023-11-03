9fbca7b15a1678126bbb1ed8bf1bb224bc3b0886
Omkar <aumkardange@gmail.com>
2023-11-03 14:21:57 +0530

    not running middelware for tests

--- packages/cli/src/lib/checkUpdate.ts
+++ packages/cli/src/lib/checkUpdate.ts
@@ -10,6 +10,9 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const checkUpdate = async () => {
+  if(process.env.NODE_ENV === 'test')
+    return
+
   const { name, version } = JSON.parse(
     fs.readFileSync(__dirname + "/../package.json").toString(),
   );
