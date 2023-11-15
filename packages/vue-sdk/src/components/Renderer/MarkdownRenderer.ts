a183f79eb3dcf3576827dbb41ba56fe7987c3d89
Andrew Glago <andrewglago1@gmail.com>
2023-11-15 20:33:29 +0000

    feat: add vue markdown smart component support

--- packages/vue-sdk/src/components/Renderer/MarkdownRenderer.ts
+++ packages/vue-sdk/src/components/Renderer/MarkdownRenderer.ts
@@ -1,7 +1,15 @@
 import type { Options as MarkdownItOptions } from "markdown-it";
 // @ts-expect-error - no types
 import MarkdownIt from "markdown-it/dist/markdown-it.js";
-import { computed, defineComponent, h, PropType } from "vue-demi";
+import {
+  computed,
+  defineComponent,
+  h,
+  PropType,
+  defineCustomElement,
+  onMounted,
+} from "vue-demi";
+import type { SmartComponentMap } from "./";
 
 const MarkdownRenderer = defineComponent({
   name: "VueMarkdown",
@@ -10,17 +18,35 @@ const MarkdownRenderer = defineComponent({
       type: String,
       required: true,
     },
-    options: {
-      type: Object as PropType<MarkdownItOptions>,
+    smartComponentMap: {
+      type: Object as PropType<SmartComponentMap>,
       default: () => ({}),
       required: false,
     },
+  },
+  setup(props) {
+    onMounted(() => {
+      const styleTags = document.querySelectorAll("style");
+      const styleLinks = document.querySelectorAll(
+        "link[rel=stylesheet]",
+      ) as NodeListOf<HTMLLinkElement>;
+
+      // Taking the web components approach means we can't use the components in SSR
+      // TODO: Figure out how to make this work with SSR
+      // https://getpantheon.atlassian.net/browse/PCC-763
+      customElements.define(
+        "pcc-component",
+        buildPccCustomElement(props.smartComponentMap, styleTags, styleLinks),
+      );
+    });
   },
   render() {
     const props = this.$props;
     const attrs = this.$attrs;
 
-    const md = new MarkdownIt(props.options);
+    const md = new MarkdownIt({
+      html: true,
+    } satisfies MarkdownItOptions);
 
     const content = computed(() => {
       const src = props.source;
@@ -33,5 +59,79 @@ const MarkdownRenderer = defineComponent({
     });
   },
 });
+
+interface ComponentProps extends Record<string, unknown> {
+  id: string;
+  attrs: string;
+  type: string;
+}
+
+const buildPccCustomElement = (
+  smartComponentMap: SmartComponentMap,
+  styles: NodeListOf<HTMLStyleElement>,
+  styleLinks: NodeListOf<HTMLLinkElement>,
+) => {
+  return defineCustomElement({
+    props: {
+      id: {
+        type: String,
+        required: true,
+      },
+      attrs: {
+        type: String,
+        required: true,
+      },
+      type: {
+        type: String,
+        required: true,
+      },
+    },
+    render(props: ComponentProps) {
+      const { attrs, type } = props;
+
+      const component = smartComponentMap?.[type];
+
+      const decodedAttrs = isomorphicBase64Decode(attrs);
+
+      return h(
+        "div",
+        {
+          class: "pcc-component",
+        },
+        [
+          // Browsers will dedupe these requests automatically
+          ...Array.from(styleLinks).map((link) =>
+            h("link", {
+              rel: "stylesheet",
+              href: link.href,
+            }),
+          ),
+          component
+            ? h(component, decodedAttrs)
+            : h("u", {}, `PCC Component - ${type}`),
+        ],
+      );
+    },
+    styles: Array.from(styles).map((style) => style.innerHTML),
+  });
+};
+
+function isomorphicBase64Decode(str: string): Record<string, unknown> {
+  try {
+    let stringifiedJSON: string | undefined;
+    if (typeof Buffer !== "undefined") {
+      stringifiedJSON = Buffer.from(str, "base64").toString("utf8");
+    } else if (typeof atob !== "undefined") {
+      stringifiedJSON = atob(str);
+    }
+
+    if (!stringifiedJSON) throw new Error("Failed to decode base64 string");
+
+    return JSON.parse(stringifiedJSON);
+  } catch (e) {
+    console.error(e);
+    return {};
+  }
+}
 
 export default MarkdownRenderer;
