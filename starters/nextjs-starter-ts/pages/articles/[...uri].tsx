0bc04735b1781dbc243ef882a804b549efb79263
Andrew Glago <andrewglago1@gmail.com>
2023-11-21 11:39:31 +0000

    feat(next-starter-ts): add preview support feat(next-starter-ts): fix api key leaking

--- starters/nextjs-starter-ts/pages/articles/[...uri].tsx
+++ starters/nextjs-starter-ts/pages/articles/[...uri].tsx
@@ -1,10 +1,96 @@
-import { ArticleRenderer } from "@pantheon-systems/pcc-react-sdk/components";
+import {
+  PantheonProvider,
+  type Article,
+} from "@pantheon-systems/pcc-react-sdk";
 import { NextSeo } from "next-seo";
 import queryString from "query-string";
+import ArticleView from "../../components/article-view";
 import Layout from "../../components/layout";
 import { Tags } from "../../components/tags";
 import { getArticleBySlugOrId } from "../../lib/Articles";
+import { buildPantheonClientWithGrant } from "../../lib/PantheonClient";
 import { pantheonAPIOptions } from "../api/pantheoncloud/[...command]";
+
+interface ArticlePageProps {
+  article: Article;
+  grant: string;
+}
+
+export default function ArticlePage({ article, grant }: ArticlePageProps) {
+  const seoMetadata = getSeoMetadata(article);
+
+  return (
+    <PantheonProvider client={buildPantheonClientWithGrant(grant)}>
+      <Layout>
+        <NextSeo
+          title={seoMetadata.title}
+          description={seoMetadata.description}
+          openGraph={{
+            type: "website",
+            title: seoMetadata.title,
+            description: seoMetadata.description,
+            article: {
+              authors: seoMetadata.authors,
+              tags: seoMetadata.tags,
+              ...(seoMetadata.publishedTime && {
+                publishedTime: seoMetadata.publishedTime,
+              }),
+            },
+          }}
+        />
+
+        <div className="max-w-screen-lg mx-auto mt-16 prose">
+          <ArticleView article={article} />
+
+          <Tags tags={article?.tags} />
+        </div>
+      </Layout>
+    </PantheonProvider>
+  );
+}
+
+export async function getServerSideProps({
+  req: { cookies },
+  query: { uri, publishingLevel, pccGrant, ...query },
+}) {
+  const slugOrId = uri[uri.length - 1];
+  const grant = pccGrant || cookies["PCC-GRANT"] || null;
+
+  const article = await getArticleBySlugOrId(
+    slugOrId,
+    publishingLevel ? publishingLevel.toString().toUpperCase() : "PRODUCTION",
+  );
+
+  if (!article) {
+    return {
+      notFound: true,
+    };
+  }
+
+  if (
+    article.slug?.trim().length &&
+    article.slug.toLowerCase() !== slugOrId?.trim().toLowerCase()
+  ) {
+    // If the article was accessed by the id rather than the slug - then redirect to the canonical
+    // link (mostly for SEO purposes than anything else).
+    return {
+      redirect: {
+        destination: queryString.stringifyUrl({
+          url: pantheonAPIOptions.resolvePath(article),
+          query: { publishingLevel, ...query },
+        }),
+        permanent: false,
+      },
+    };
+  }
+
+  return {
+    props: {
+      article,
+      grant,
+    },
+  };
+}
 
 interface DateInputObject {
   msSinceEpoch: string;
@@ -34,102 +120,3 @@ const getSeoMetadata = (article) => {
     publishedTime,
   };
 };
-
-export default function PageTemplate({ article }) {
-  const seoMetadata = getSeoMetadata(article);
-  return (
-    <Layout>
-      <NextSeo
-        title={seoMetadata.title}
-        description={seoMetadata.description}
-        openGraph={{
-          type: "website",
-          title: seoMetadata.title,
-          description: seoMetadata.description,
-          article: {
-            authors: seoMetadata.authors,
-            tags: seoMetadata.tags,
-            ...(seoMetadata.publishedTime && {
-              publishedTime: seoMetadata.publishedTime,
-            }),
-          },
-        }}
-      />
-
-      <div className="max-w-screen-lg mx-auto mt-16 prose">
-        <ArticleRenderer
-          article={article}
-          renderTitle={(titleElement) => (
-            <div>
-              <h1 className="text-3xl font-bold md:text-4xl">{titleElement}</h1>
-
-              {article.updatedAt ? (
-                <p className="py-2">
-                  Last Updated:{" "}
-                  {new Date(article.updatedAt).toLocaleDateString("en-US", {
-                    year: "numeric",
-                    month: "long",
-                    day: "numeric",
-                  })}
-                </p>
-              ) : null}
-
-              <hr className="mt-6 mb-8" />
-            </div>
-          )}
-          smartComponentMap={pantheonAPIOptions.smartComponentMap}
-        />
-        <Tags tags={article?.tags} />
-      </div>
-    </Layout>
-  );
-}
-
-export async function getServerSideProps({
-  req: { cookies },
-  res,
-  query: { uri, publishingLevel, pccGrant, ...query },
-}) {
-  const slugOrId = uri[uri.length - 1];
-  const article = await getArticleBySlugOrId(
-    slugOrId,
-    pccGrant || cookies["PCC-GRANT"],
-    publishingLevel ? publishingLevel.toString().toUpperCase() : "PRODUCTION",
-  );
-
-  if (pccGrant) {
-    res.setHeader("Set-Cookie", `PCC-GRANT=${pccGrant}; Path=/; SameSite=Lax`);
-  }
-
-  if (!article) {
-    return {
-      notFound: true,
-    };
-  } else if (
-    article.slug?.trim().length &&
-    article.slug.toLowerCase() !== slugOrId?.trim().toLowerCase()
-  ) {
-    // If the article was accessed by the id rather than the slug - then redirect to the canonical
-    // link (mostly for SEO purposes than anything else).
-    return {
-      redirect: {
-        destination: `${pantheonAPIOptions.resolvePath(
-          article,
-        )}?${queryString.stringify({ publishingLevel, ...query })}#`,
-        permanent: false,
-      },
-    };
-  }
-
-  if (!article) {
-    return {
-      notFound: true,
-    };
-  }
-
-  return {
-    props: {
-      article,
-    },
-  };
-}
