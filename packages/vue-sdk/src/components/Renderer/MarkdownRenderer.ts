5bc91c7a62de09cb6e7ae95fb9e6e6cbb96545a9
Omkar <aumkardange@gmail.com>
2023-12-08 15:58:24 +0530

    Replaced markdown-it with remark

--- packages/vue-sdk/src/components/Renderer/MarkdownRenderer.ts
+++ packages/vue-sdk/src/components/Renderer/MarkdownRenderer.ts
@@ -1,7 +1,3 @@
-import type { Options as MarkdownItOptions, Token } from "markdown-it";
-import MarkdownIt from "markdown-it";
-import MarkdownItAnchor from "markdown-it-anchor";
-import MarkdownItAttrs from "markdown-it-attrs";
 import {
   computed,
   defineComponent,
@@ -11,6 +7,86 @@ import {
   onMounted,
 } from "vue-demi";
 import type { ComponentMap, SmartComponentMap } from "./";
+import { unified } from "unified";
+import remarkParse from "remark-parse";
+import remarkRehype from "remark-rehype";
+import remarkHeadingId from "remark-heading-id";
+import rehypeRaw from "rehype-raw";
+import { toJsxRuntime } from "hast-util-to-jsx-runtime";
+import { Fragment, jsx } from "vue/jsx-runtime";
+import type { UnistParent } from "unist-util-visit/lib";
+import { visit } from "unist-util-visit";
+
+function fixComponentParentRehypePlugin() {
+  return (tree: UnistParent) => {
+    visit(
+      tree,
+      { type: "element", tagName: "pcc-component" },
+      (node, _, parent) => {
+        if (
+          parent &&
+          "tagName" in parent &&
+          // @ts-expect-error TODO: Type this properly
+          parent.tagName !== "div"
+        ) {
+          // @ts-expect-error TODO: Type this properly
+          parent.tagName = "div";
+        }
+      },
+    );
+  };
+}
+
+// TODO: Check if need URL transform
+// export function urlTransform(value: any) {
+//   // Same as:
+//   // <https://github.com/micromark/micromark/blob/929275e/packages/micromark-util-sanitize-uri/dev/index.js#L34>
+//   // But without the `encode` part.
+//   const colon = value.indexOf(":");
+//   const questionMark = value.indexOf("?");
+//   const numberSign = value.indexOf("#");
+//   const slash = value.indexOf("/");
+
+//   if (
+//     // If there is no protocol, it’s relative.
+//     colon < 0 ||
+//     // If the first colon is after a `?`, `#`, or `/`, it’s not a protocol.
+//     (slash > -1 && colon > slash) ||
+//     (questionMark > -1 && colon > questionMark) ||
+//     (numberSign > -1 && colon > numberSign) ||
+//     // It is a protocol, it should be allowed.
+//     safeProtocol.test(value.slice(0, colon))
+//   ) {
+//     return value;
+//   }
+
+//   return "";
+// }
+
+// function transform(node: any, index: any, parent: any) {
+//   if (node.type === "raw" && parent && typeof index === "number") {
+//     parent.children[index] = { type: "text", value: node.value };
+//     return index;
+//   }
+
+//   if (node.type === "element") {
+//     /** @type {string} */
+//     let key;
+
+//     for (key in urlAttributes) {
+//       if (
+//         Object.hasOwn(urlAttributes, key) &&
+//         Object.hasOwn(node.properties, key)
+//       ) {
+//         const value = node.properties[key];
+//         const test = urlAttributes[key];
+//         if (test === null || test.includes(node.tagName)) {
+//           node.properties[key] = urlTransform(String(value || ""));
+//         }
+//       }
+//     }
+//   }
+// }
 
 const MarkdownRenderer = defineComponent({
   name: "VueMarkdown",
@@ -50,20 +126,35 @@ const MarkdownRenderer = defineComponent({
     const props = this.$props;
     const attrs = this.$attrs;
 
-    const md = new MarkdownIt({
-      html: true,
-    } satisfies MarkdownItOptions);
-    md.use(MarkdownItAttrs);
-    md.use(MarkdownItAnchor);
     const content = computed(() => {
-      const src = props.source;
-      return md?.render(src);
-    });
+      const processor = unified()
+        .use(remarkParse)
+        .use(remarkHeadingId)
+        .use(remarkRehype, { allowDangerousHtml: true })
+        .use(rehypeRaw as any, { allowDangerousHtml: true })
+        .use(fixComponentParentRehypePlugin);
 
-    return h("div", {
-      ...attrs,
-      innerHTML: content.value,
+      const mdastTree = processor.parse(props.source);
+      const hastTree = processor.runSync(mdastTree);
+      // TODO: Check if we need transform
+      // visit(hastTree, transform)
+      return hastTree;
     });
+    return h(
+      "div",
+      {
+        ...attrs,
+      },
+      toJsxRuntime(content.value, {
+        Fragment,
+        components: {
+          ["pcc-component" as "div"]: props.smartComponentMap.LEAD_CAPTURE,
+        },
+        // TODO: Fix type warnings
+        jsx: jsx,
+        jsxs: jsx,
+      }),
+    );
   },
 });
 
