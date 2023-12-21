a7064aab734c8a37746f8abd67787c5c4afcfb90
Andrew Glago <andrewglago1@gmail.com>
2023-12-21 06:18:24 +0000

    fix: correct build config for React

--- packages/react-sample-library/tsup.config.ts
+++ packages/react-sample-library/tsup.config.ts
@@ -6,17 +6,8 @@ export default defineConfig({
   clean: true,
   dts: true,
   splitting: false,
-  target: "es2019",
   outDir: "dist",
-  globalName: "PccVue",
   entry: ["src/index.ts"],
   format: ["cjs", "esm"],
-  external: [],
-  outExtension: ({ format }) => {
-    if (format === "iife") return { js: ".global.js" };
-    if (format === "cjs") return { js: ".cjs" };
-    if (format === "esm") return { js: ".mjs" };
-    return { js: ".js" };
-  },
-  esbuildPlugins: [],
+  external: ["react"],
 });
