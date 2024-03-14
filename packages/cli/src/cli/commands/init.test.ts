0aca439d5642a40821a3113b969c61d016fe9eb7
Omkar <aumkardange@gmail.com>
2024-03-14 17:29:03 +0530

    testing

--- packages/cli/src/cli/commands/init.test.ts
+++ packages/cli/src/cli/commands/init.test.ts
@@ -30,52 +30,52 @@ const executePCC = async (command: string, args: string[]) => {
   }
 };
 
-test("should be able to init starter kit for nextjs template", async () => {
-  const appFolder = tmp.tmpNameSync();
+// test("should be able to init starter kit for nextjs template", async () => {
+//   const appFolder = tmp.tmpNameSync();
 
-  await executePCC("init", [appFolder, "--template", "nextjs", "--use-pnpm"]);
+//   await executePCC("init", [appFolder, "--template", "nextjs", "--use-pnpm"]);
 
-  // Eslint not initialized
-  expect(fs.existsSync(`${appFolder}/.eslintrc.json`)).toBe(false);
+//   // Eslint not initialized
+//   expect(fs.existsSync(`${appFolder}/.eslintrc.json`)).toBe(false);
 
-  // Checking if primary required files for Nextjs starter kit are created.
-  expect(fs.existsSync(`${appFolder}/next.config.js`)).toBe(true);
-  expect(fs.existsSync(`${appFolder}/pages/index.jsx`)).toBe(true);
+//   // Checking if primary required files for Nextjs starter kit are created.
+//   expect(fs.existsSync(`${appFolder}/next.config.js`)).toBe(true);
+//   expect(fs.existsSync(`${appFolder}/pages/index.jsx`)).toBe(true);
 
-  // package.json checks
-  expect(fs.existsSync(`${appFolder}/package.json`)).toBe(true);
-  const packageJson = JSON.parse(
-    readFileSync(`${appFolder}/package.json`).toString(),
-  );
-  expect(packageJson.name).toBe(path.parse(appFolder).base);
+//   // package.json checks
+//   expect(fs.existsSync(`${appFolder}/package.json`)).toBe(true);
+//   const packageJson = JSON.parse(
+//     readFileSync(`${appFolder}/package.json`).toString(),
+//   );
+//   expect(packageJson.name).toBe(path.parse(appFolder).base);
 
-  // Remove app folder
-  fs.rmSync(appFolder, { recursive: true, force: true });
-});
+//   // Remove app folder
+//   fs.rmSync(appFolder, { recursive: true, force: true });
+// });
 
-test("should be able to init starter kit for gatsby template", async () => {
-  const appFolder = tmp.tmpNameSync();
+// test("should be able to init starter kit for gatsby template", async () => {
+//   const appFolder = tmp.tmpNameSync();
 
-  await executePCC("init", [appFolder, "--template", "gatsby", "--use-pnpm"]);
+//   await executePCC("init", [appFolder, "--template", "gatsby", "--use-pnpm"]);
 
-  // Eslint not initialized
-  expect(fs.existsSync(`${appFolder}/.eslintrc.json`)).toBe(false);
+//   // Eslint not initialized
+//   expect(fs.existsSync(`${appFolder}/.eslintrc.json`)).toBe(false);
 
-  // Checking if primary required files for Gatsby starter kit are created.
-  expect(fs.existsSync(`${appFolder}/gatsby-browser.js`)).toBe(true);
-  expect(fs.existsSync(`${appFolder}/gatsby-config.js`)).toBe(true);
-  expect(fs.existsSync(`${appFolder}/gatsby-browser.js`)).toBe(true);
+//   // Checking if primary required files for Gatsby starter kit are created.
+//   expect(fs.existsSync(`${appFolder}/gatsby-browser.js`)).toBe(true);
+//   expect(fs.existsSync(`${appFolder}/gatsby-config.js`)).toBe(true);
+//   expect(fs.existsSync(`${appFolder}/gatsby-browser.js`)).toBe(true);
 
-  // package.json checks
-  expect(fs.existsSync(`${appFolder}/package.json`)).toBe(true);
-  const packageJson = JSON.parse(
-    readFileSync(`${appFolder}/package.json`).toString(),
-  );
-  expect(packageJson.name).toBe(path.parse(appFolder).base);
+//   // package.json checks
+//   expect(fs.existsSync(`${appFolder}/package.json`)).toBe(true);
+//   const packageJson = JSON.parse(
+//     readFileSync(`${appFolder}/package.json`).toString(),
+//   );
+//   expect(packageJson.name).toBe(path.parse(appFolder).base);
 
-  // Remove app folder
-  fs.rmSync(appFolder, { recursive: true, force: true });
-});
+//   // Remove app folder
+//   fs.rmSync(appFolder, { recursive: true, force: true });
+// });
 
 test("should be able to init starter kit for nextjs template with typescript", async () => {
   const appFolder = tmp.tmpNameSync();
@@ -102,107 +102,107 @@ test("should be able to init starter kit for nextjs template with typescript", a
   );
   expect(packageJson.name).toBe(path.parse(appFolder).base);
 
-  await sh("cd", [appFolder]);
+  await sh("cd", [appFolder], true);
   writeFileSync(".env.local", ENV_CONTENT);
-  await sh("pnpm", ["build"]);
-  await sh("cd", [".."]);
+  await sh("pnpm", ["build"], true);
+  await sh("cd", [".."], true);
   // Remove app folder
   fs.rmSync(appFolder, { recursive: true, force: true });
 });
 
-test("should be able to init starter kit for gatsby template with typescript", async () => {
-  const appFolder = tmp.tmpNameSync();
+// test("should be able to init starter kit for gatsby template with typescript", async () => {
+//   const appFolder = tmp.tmpNameSync();
 
-  await executePCC("init", [appFolder, "--template", "gatsby", "--ts"]);
+//   await executePCC("init", [appFolder, "--template", "gatsby", "--ts"]);
 
-  // Check that TypesScript source files exist.
-  expect(fs.existsSync(`${appFolder}/src/templates/index.tsx`)).toBe(true);
+//   // Check that TypesScript source files exist.
+//   expect(fs.existsSync(`${appFolder}/src/templates/index.tsx`)).toBe(true);
 
-  // Eslint not initialized
-  expect(fs.existsSync(`${appFolder}/.eslintrc.json`)).toBe(false);
+//   // Eslint not initialized
+//   expect(fs.existsSync(`${appFolder}/.eslintrc.json`)).toBe(false);
 
-  // Checking if primary required files for Gatsby starter kit are created
-  expect(fs.existsSync(`${appFolder}/gatsby-browser.js`)).toBe(true);
-  expect(fs.existsSync(`${appFolder}/gatsby-config.js`)).toBe(true);
-  expect(fs.existsSync(`${appFolder}/gatsby-browser.js`)).toBe(true);
+//   // Checking if primary required files for Gatsby starter kit are created
+//   expect(fs.existsSync(`${appFolder}/gatsby-browser.js`)).toBe(true);
+//   expect(fs.existsSync(`${appFolder}/gatsby-config.js`)).toBe(true);
+//   expect(fs.existsSync(`${appFolder}/gatsby-browser.js`)).toBe(true);
 
-  // package.json checks
-  expect(fs.existsSync(`${appFolder}/package.json`)).toBe(true);
-  const packageJson = JSON.parse(
-    readFileSync(`${appFolder}/package.json`).toString(),
-  );
-  expect(packageJson.name).toBe(path.parse(appFolder).base);
+//   // package.json checks
+//   expect(fs.existsSync(`${appFolder}/package.json`)).toBe(true);
+//   const packageJson = JSON.parse(
+//     readFileSync(`${appFolder}/package.json`).toString(),
+//   );
+//   expect(packageJson.name).toBe(path.parse(appFolder).base);
 
-  // Remove app folder
-  fs.rmSync(appFolder, { recursive: true, force: true });
-});
+//   // Remove app folder
+//   fs.rmSync(appFolder, { recursive: true, force: true });
+// });
 
-test("should be able to init starter kit with eslint and app name", async () => {
-  const appFolder = tmp.tmpNameSync();
+// test("should be able to init starter kit with eslint and app name", async () => {
+//   const appFolder = tmp.tmpNameSync();
 
-  await executePCC("init", [
-    appFolder,
-    "--appName",
-    "test_app",
-    "--template",
-    "nextjs",
-    "--eslint",
-  ]);
+//   await executePCC("init", [
+//     appFolder,
+//     "--appName",
+//     "test_app",
+//     "--template",
+//     "nextjs",
+//     "--eslint",
+//   ]);
 
-  // Eslint is initialized
-  expect(fs.existsSync(`${appFolder}/.eslintrc.json`)).toBe(true);
+//   // Eslint is initialized
+//   expect(fs.existsSync(`${appFolder}/.eslintrc.json`)).toBe(true);
 
-  // Package.json checks
-  expect(fs.existsSync(`${appFolder}/package.json`)).toBe(true);
-  const packageJson = JSON.parse(
-    readFileSync(`${appFolder}/package.json`).toString(),
-  );
-  expect(packageJson.name).toBe("test_app");
+//   // Package.json checks
+//   expect(fs.existsSync(`${appFolder}/package.json`)).toBe(true);
+//   const packageJson = JSON.parse(
+//     readFileSync(`${appFolder}/package.json`).toString(),
+//   );
+//   expect(packageJson.name).toBe("test_app");
 
-  // Remove app folder
-  fs.rmSync(appFolder, { recursive: true, force: true });
-});
+//   // Remove app folder
+//   fs.rmSync(appFolder, { recursive: true, force: true });
+// });
 
-test("should raise error when project directory already exists", async () => {
-  const appFolder = tmp.tmpNameSync();
-  fs.mkdirSync(appFolder);
+// test("should raise error when project directory already exists", async () => {
+//   const appFolder = tmp.tmpNameSync();
+//   fs.mkdirSync(appFolder);
 
-  let error = false;
-  try {
-    await executePCC("init", [appFolder, "--template", "nextjs", "--eslint"]);
-  } catch (err) {
-    error = true;
-  }
+//   let error = false;
+//   try {
+//     await executePCC("init", [appFolder, "--template", "nextjs", "--eslint"]);
+//   } catch (err) {
+//     error = true;
+//   }
 
-  // No files are created
-  expect(error).toBe(true);
+//   // No files are created
+//   expect(error).toBe(true);
 
-  // Remove app folder
-  fs.rmSync(appFolder, { recursive: true, force: true });
-});
+//   // Remove app folder
+//   fs.rmSync(appFolder, { recursive: true, force: true });
+// });
 
-test("should raise error when template name is incorrect", async () => {
-  const appFolder = tmp.tmpNameSync();
+// test("should raise error when template name is incorrect", async () => {
+//   const appFolder = tmp.tmpNameSync();
 
-  let error = 0;
-  try {
-    await executePCC("init", [
-      appFolder,
-      "--appName",
-      "test_app",
-      "--template",
-      "react",
-      "--eslint",
-    ]);
-  } catch (err) {
-    error = 1;
-  }
+//   let error = 0;
+//   try {
+//     await executePCC("init", [
+//       appFolder,
+//       "--appName",
+//       "test_app",
+//       "--template",
+//       "react",
+//       "--eslint",
+//     ]);
+//   } catch (err) {
+//     error = 1;
+//   }
 
-  expect(error).toBe(1);
+//   expect(error).toBe(1);
 
-  // No files are created
-  expect(fs.existsSync(appFolder)).toBe(false);
+//   // No files are created
+//   expect(fs.existsSync(appFolder)).toBe(false);
 
-  // Remove app folder
-  fs.rmSync(appFolder, { recursive: true, force: true });
-});
+//   // Remove app folder
+//   fs.rmSync(appFolder, { recursive: true, force: true });
+// });
