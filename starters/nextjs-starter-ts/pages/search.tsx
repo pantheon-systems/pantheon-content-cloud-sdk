0ef1e14861d4145d8ff297bc23b004d897a2f4d8
Kevin Stubbs <kevin@pubgenius.io>
2024-03-29 19:12:37 +0300

    Remove debug log

--- starters/nextjs-starter-ts/pages/search.tsx
+++ starters/nextjs-starter-ts/pages/search.tsx
@@ -1,4 +1,7 @@
-import { ArticleWithoutContent, PCCConvenienceFunctions } from "@pantheon-systems/pcc-react-sdk";
+import {
+  ArticleWithoutContent,
+  PCCConvenienceFunctions,
+} from "@pantheon-systems/pcc-react-sdk";
 import { useAtom } from "jotai";
 import { NextSeo } from "next-seo";
 import Image from "next/image";
@@ -92,8 +95,6 @@ export async function getServerSideProps({ query }) {
       },
       true,
     );
-
-  console.log({ articles, summary });
 
   return {
     props: {
