e2e5b0c9f31f95f97db3dfd232919e4569c02640
Andrew Glago <andrewglago1@gmail.com>
2024-03-29 20:19:45 +0000

    feat: update preview and publish process

--- packages/cli/src/cli/commands/documents.ts
+++ packages/cli/src/cli/commands/documents.ts
@@ -1,61 +1,16 @@
 import { exit } from "process";
 import chalk from "chalk";
 import AddOnApiHelper from "../../lib/addonApiHelper";
-import config from "../../lib/config";
 import { Logger, SpinnerLogger } from "../../lib/logger";
-import { filterUndefinedProperties, parameterize } from "../../lib/utils";
 import { errorHandler } from "../exceptions";
 
 type GeneratePreviewParam = {
   documentId: string;
   baseUrl: string;
 };
-function generateBaseAPIPath(siteData: Site, baseUrl?: string) {
-  if (!siteData) return "#";
-
-  const isPlayground = siteData.__isPlayground;
-
-  let _baseUrl: string;
-  if (baseUrl) _baseUrl = baseUrl;
-  else if (isPlayground) _baseUrl = config.playgroundUrl;
-  else _baseUrl = siteData.url;
-
-  return isPlayground
-    ? `${_baseUrl}/api/${siteData.id}/pantheoncloud`
-    : `${_baseUrl}/api/pantheoncloud`;
-}
-
-async function generateDocumentPath(
-  site: Site,
-  docId: string,
-  isPreview: boolean,
-  {
-    baseUrl,
-    queryParams,
-  }: {
-    baseUrl?: string;
-    queryParams?: Record<string, string>;
-  },
-) {
-  const augmentedQueryParams = { ...queryParams };
-
-  if (isPreview) {
-    augmentedQueryParams.pccGrant = await AddOnApiHelper.getPreviewJwt(docId);
-  }
-
-  const params =
-    augmentedQueryParams == null
-      ? {}
-      : filterUndefinedProperties(augmentedQueryParams);
-
-  return `${generateBaseAPIPath(site, baseUrl)}/document/${docId}${
-    Object.values(params).length > 0 ? `/?${parameterize(params)}` : ""
-  }`;
-}
 
 export const generatePreviewLink = errorHandler<GeneratePreviewParam>(
   async ({ documentId, baseUrl }: GeneratePreviewParam) => {
-    let document: Article;
     const logger = new Logger();
 
     if (baseUrl) {
@@ -71,47 +26,19 @@ export const generatePreviewLink = errorHandler<GeneratePreviewParam>(
       }
     }
 
-    // Fetching document details
-    const fetchLogger = new SpinnerLogger("Fetching document details...");
-    fetchLogger.start();
-    try {
-      document = await AddOnApiHelper.getDocument(documentId);
-    } catch (err) {
-      fetchLogger.stop();
-      if ((err as { response?: { status: number } }).response?.status === 404) {
-        logger.error(
-          chalk.red("ERROR: Article not found for given document ID."),
-        );
-        exit(1);
-      } else throw err;
-    }
-    let site: Site;
-    try {
-      site = await AddOnApiHelper.getSite(document.siteId);
-    } catch (err) {
-      fetchLogger.stop();
-      if ((err as { response: { status: number } }).response.status === 404) {
-        logger.error(chalk.red("ERROR: Site not found for given document."));
-        exit(1);
-      } else throw err;
-    }
-    fetchLogger.succeed("Fetched document details!");
-
     // Generating link
     const generateLinkLogger = new SpinnerLogger("Generating preview link");
     generateLinkLogger.start();
 
-    const buildLink = `${await generateDocumentPath(site, documentId, true, {
-      queryParams: {
-        publishingLevel: "REALTIME",
-      },
+    const previewLink = await AddOnApiHelper.previewFile(documentId, {
       baseUrl,
-    })}`;
+    });
+
     generateLinkLogger.succeed(
       "Successfully generated preview link. Please copy it from below:",
     );
 
-    logger.log(chalk.green(buildLink));
+    logger.log(chalk.green(previewLink));
   },
 );
 export const DOCUMENT_EXAMPLES = [
