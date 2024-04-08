ecfe724171dbbc668655f3b21223d2658d8f3f3c
Kevin Stubbs <kevin@pubgenius.io>
2024-04-08 14:20:57 +0300

    SDK and starter kit support showing snippet for search results.

--- starters/nextjs-starter-ts/components/grid.tsx
+++ starters/nextjs-starter-ts/components/grid.tsx
@@ -12,57 +12,47 @@ interface Props {
   altText?: string;
   tags?: string[];
   title: string;
+  snippet?: string;
 }
 
-const GridItem = ({ href, imgSrc, altText, tags, title }: Props) => {
+const GridItem = ({ href, imgSrc, altText, tags, title, snippet }: Props) => {
   return (
-    <>
-      <div className="flex flex-col h-full overflow-hidden rounded-lg shadow-lg">
-        <Link passHref href={href}>
-          <div className="relative flex-shrink-0 h-40 cursor-pointer hover:border-indigo-500 border-2s not-prose">
-            {imgSrc != null ? (
-              <img
-                src={imgSrc}
-                alt={altText || title}
-                className="object-cover w-full h-full"
-              />
-            ) : (
-              <GradientPlaceholder />
-            )}
-          </div>
-        </Link>
-        <div className="mx-6 my-4 text-xl font-semibold leading-7 text-gray-900">
-          <Link passHref href={href}>
-            <div className="hover:scale-105">{title} &rarr;</div>
-          </Link>
-          <Tags tags={tags || []} />
+    <div className="flex flex-col h-full overflow-hidden rounded-lg shadow-lg">
+      <Link passHref href={href}>
+        <div className="relative flex-shrink-0 h-40 cursor-pointer hover:border-indigo-500 border-2s not-prose">
+          {imgSrc != null ? (
+            <img
+              src={imgSrc}
+              alt={altText || title}
+              className="object-cover w-full h-full"
+            />
+          ) : (
+            <GradientPlaceholder />
+          )}
+          {snippet != null ? <div>{snippet.substring(0, 120)}</div> : null}
         </div>
+      </Link>
+      <div className="mx-6 my-4 text-xl font-semibold leading-7 text-gray-900">
+        <Link passHref href={href}>
+          <div className="hover:scale-105">{title} &rarr;</div>
+        </Link>
+        <Tags tags={tags || []} />
       </div>
-    </>
+    </div>
   );
 };
 
-const PostGridItem = ({ content: article }) => {
+const ArticleGridItem = ({ content: article }) => {
   return (
     <GridItem
       href={`/articles/${article.slug || article.id}`}
       imgSrc={article.metadata?.["Hero Image"]}
       title={article.title}
       tags={article.tags}
+      snippet={article.snippet}
     />
   );
 };
 
-const PageGridItem = ({ content: article }) => {
-  return (
-    <GridItem
-      href={`/articles/${article.slug || article.id}`}
-      imgSrc={article.metadata?.["Hero Image"]}
-      title={article.title}
-      tags={article.tags}
-    />
-  );
-};
-
-export const PostGrid = withGrid(PostGridItem);
-export const PageGrid = withGrid(PageGridItem);
+export const PostGrid = withGrid(ArticleGridItem);
+export const PageGrid = PostGrid;
