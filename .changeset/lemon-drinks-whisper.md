c8800c80faf71606b051549b8e201fa44b616b55
Kevin Stubbs <kevin@pubgenius.io>
2024-05-06 12:30:52 +0400

    Add preserveImageStyles experimental flag for article rendering.

---
+++ .changeset/lemon-drinks-whisper.md
@@ -0,0 +1,7 @@
+---
+"@pantheon-systems/next-approuter-pcc-starter-ts": patch
+"@pantheon-systems/pcc-react-sdk": patch
+"@pantheon-systems/pcc-vue-sdk": patch
+---
+
+Add preserveImageStyles experimental flag for article rendering.
--- packages/react-sdk/src/components/ArticleRenderer/index.tsx
+++ packages/react-sdk/src/components/ArticleRenderer/index.tsx
@@ -57,6 +57,7 @@ interface Props {
   renderBody?: (bodyElement: React.ReactElement) => React.ReactNode;
   __experimentalFlags?: {
     disableAllStyles?: boolean;
+    preserveImageStyles?: boolean;
     disableDefaultErrorBoundaries?: boolean;
     useUnintrusiveTitleRendering?: boolean;
   };
@@ -149,6 +150,7 @@ const ArticleRenderer = ({
       element: titleContent,
       componentMap,
       smartComponentMap,
+      preserveImageStyles: !!__experimentalFlags?.preserveImageStyles,
       disableAllStyles: !!__experimentalFlags?.disableAllStyles,
       disableDefaultErrorBoundaries:
         !!__experimentalFlags?.disableDefaultErrorBoundaries,
@@ -165,6 +167,7 @@ const ArticleRenderer = ({
           smartComponentMap,
           componentMap,
           disableAllStyles: !!__experimentalFlags?.disableAllStyles,
+          preserveImageStyles: !!__experimentalFlags?.preserveImageStyles,
           disableDefaultErrorBoundaries:
             !!__experimentalFlags?.disableDefaultErrorBoundaries,
         }),
