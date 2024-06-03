88faf0fbaa7f6a5b186de74925c71c73aeb48431
Andrew Glago <andrewglago1@gmail.com>
2024-06-03 18:07:11 +0000

    refactor: remove testing changes

--- packages/react-sdk/src/server/index.ts
+++ packages/react-sdk/src/server/index.ts
@@ -16,4 +16,6 @@ export {
 } from "@pantheon-systems/pcc-sdk-core";
 export * from "@pantheon-systems/pcc-sdk-core/types";
 
+export * from "../core/pantheon-context";
+
 export { NextPantheonAPI as PantheonAPI } from "../core/pantheon-api";
