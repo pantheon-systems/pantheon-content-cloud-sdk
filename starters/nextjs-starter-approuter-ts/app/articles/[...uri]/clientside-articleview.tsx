766468606142b1a568ec2182acb6c725fd48d3f2
Kevin Stubbs <kevin@pubgenius.io>
2024-06-03 21:11:57 +0700

    Update SDK. Use updateConfig to get around .env issue

--- starters/nextjs-starter-approuter-ts/app/articles/[...uri]/clientside-articleview.tsx
+++ starters/nextjs-starter-approuter-ts/app/articles/[...uri]/clientside-articleview.tsx
@@ -4,8 +4,15 @@ import {
   Article,
   PantheonProvider,
   PCCConvenienceFunctions,
+  updateConfig,
 } from "@pantheon-systems/pcc-react-sdk";
 import ArticleView from "../../../components/article-view";
+
+updateConfig({
+  pccHost: (process.env.PCC_HOST || process.env.NEXT_PUBLIC_PCC_HOST) as string,
+  siteId: (process.env.PCC_SITE_ID ||
+    process.env.NEXT_PUBLIC_PCC_SITE_ID) as string,
+});
 
 export default function ClientsideArticleView({
   grant,
