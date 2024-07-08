fc9b0dd2aa3cc0ee5d9cee34830fed58c2278d1b
Kevin Stubbs <kevin@pubgenius.io>
2024-07-08 13:54:51 +0700

    Add delete site command. More human-friendly 404 message for admin commands.

---
+++ .changeset/tough-zebras-pull.md
@@ -0,0 +1,6 @@
+---
+"@pantheon-systems/pcc-cli": minor
+---
+
+New command: site delete.
+Improved error messages for incorrect/invalid site id for admin actions.
--- packages/cli/src/cli/commands/sites/admins.ts
+++ packages/cli/src/cli/commands/sites/admins.ts
@@ -1,3 +1,5 @@
+import axios from "axios";
+import chalk from "chalk";
 import ora from "ora";
 import AddOnApiHelper from "../../../lib/addonApiHelper";
 import { errorHandler } from "../../exceptions";
@@ -5,27 +7,51 @@ import { errorHandler } from "../../exceptions";
 type listAdminsSchemaParams = { siteId: string };
 export const listAdminsSchema = errorHandler<listAdminsSchemaParams>(
   async ({ siteId }: listAdminsSchemaParams) => {
-    const spinner = ora("Retrieving admins...").start();
-    const result = await AddOnApiHelper.listAdmins(siteId);
-    spinner.succeed();
-    console.log(JSON.stringify(result, null, 4));
+    try {
+      const spinner = ora("Retrieving admins...").start();
+      const result = await AddOnApiHelper.listAdmins(siteId);
+      spinner.succeed();
+      console.log(JSON.stringify(result, null, 4));
+    } catch (e) {
+      if (axios.isAxiosError(e) && e.response?.status === 404) {
+        console.log(chalk.red(`\nSite ID not recognized`));
+      } else {
+        throw e;
+      }
+    }
   },
 );
 
 type removeAdminSchemaParams = { siteId: string; email: string };
 export const removeAdminSchema = errorHandler<removeAdminSchemaParams>(
   async ({ siteId, email }: removeAdminSchemaParams) => {
-    const spinner = ora("Removing admin...").start();
-    await AddOnApiHelper.removeAdmin(siteId, email);
-    spinner.succeed();
+    try {
+      const spinner = ora("Removing admin...").start();
+      await AddOnApiHelper.removeAdmin(siteId, email);
+      spinner.succeed();
+    } catch (e) {
+      if (axios.isAxiosError(e) && e.response?.status === 404) {
+        console.log(chalk.red(`\nSite ID not recognized`));
+      } else {
+        throw e;
+      }
+    }
   },
 );
 
 type addAdminSchemaParams = { siteId: string; email: string };
 export const addAdminSchema = errorHandler<addAdminSchemaParams>(
   async ({ siteId, email }: addAdminSchemaParams) => {
-    const spinner = ora("Adding admin...").start();
-    await AddOnApiHelper.addAdmin(siteId, email);
-    spinner.succeed();
+    try {
+      const spinner = ora("Adding admin...").start();
+      await AddOnApiHelper.addAdmin(siteId, email);
+      spinner.succeed();
+    } catch (e) {
+      if (axios.isAxiosError(e) && e.response?.status === 404) {
+        console.log(chalk.red(`\nSite ID not recognized`));
+      } else {
+        throw e;
+      }
+    }
   },
 );
