19473cf6164714f292b8df697723ab8dbd13bd56
Omkar <aumkardange@gmail.com>
2024-08-20 19:31:31 +0530

    reverting change

--- starters/vue-starter-ts/pages/articles/[id].vue
+++ starters/vue-starter-ts/pages/articles/[id].vue
@@ -2,22 +2,27 @@
 import ArticlePreview from '../../components/article/ArticlePreview.vue';
 import ArticleView from '../../components/article/ArticleView.vue';
 import { type Article, type ArticleWithoutContent, type PantheonClientConfig } from '@pantheon-systems/pcc-vue-sdk';
+
 const route = useRoute()
 const config = useRuntimeConfig()
+
 const { publishingLevel } = route.query
 const pccSiteId = config.public.siteId
 const pccGrant = (route.query.pccGrant?.toString() ?? useCookie('PCC-GRANT').value) || undefined
+
 const pantheonConfig = {
   siteId: pccSiteId,
   pccGrant,
   apiKey: "not-needed-on-client",
 } satisfies PantheonClientConfig
+
 const articleId = route.params.id
 const { data, error } = await useFetch<{article:Article, recommendedArticles: ArticleWithoutContent[]}>(`/api/articles/${articleId}`, {
   query: {
     publishingLevel
   }
 })
+
 const isPreview = pccGrant && publishingLevel === "REALTIME";
 </script>
 
