a0e65c95c04cae15257735db1782314cc19f155b
Andrew Glago <andrewglago1@gmail.com>
2024-08-26 15:14:57 +0000

    refactor: update log

--- starters/nextjs-starter-approuter-ts/app/search/error.tsx
+++ starters/nextjs-starter-approuter-ts/app/search/error.tsx
@@ -3,8 +3,13 @@
 import Layout from "../../components/layout";
 import SearchResults from "./search-results";
 
-export default function SearchError() {
-  console.log("Error loading search results");
+export default function SearchError({
+  error,
+}: {
+  error: Error & { digest?: string };
+}) {
+  console.error(error);
+
   return (
     <Layout>
       <SearchResults searchResults={null} error />
