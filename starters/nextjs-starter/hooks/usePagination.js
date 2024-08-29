00d3369e25d58784d7095ec0db65e61fbdd4c0e8
Omkar <aumkardange@gmail.com>
2024-08-29 21:31:55 +0530

    using useSWR inside pagination hook

--- starters/nextjs-starter/hooks/usePagination.js
+++ starters/nextjs-starter/hooks/usePagination.js
@@ -1,41 +1,49 @@
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
+import useSWR from "swr";
 
 export function usePagination({ cursor, initialArticles, pageSize }) {
-  const [currentCursor, setCurrentCursor] = useState(cursor);
   const [articlePages, setArticlePages] = useState(
     initialArticles ? [initialArticles] : [],
   );
-  const [fetching, setFetching] = useState(false);
   const [currentPage, setCurrentPage] = useState(0);
+  const [currentCursor, setCurrentCursor] = useState(cursor);
+
+  const fetcher = useCallback(
+    async (key) => {
+      const pageNumber = Number(key);
+      if (articlePages[pageNumber]) return null;
+
+      const response = await fetch(
+        `/api/utils/paginate?pageSize=${pageSize}&cursor=${currentCursor}`,
+      );
+      return await response.json();
+    },
+    [currentCursor, pageSize, articlePages],
+  );
+
+  const { data: newResponse, isLoading } = useSWR(
+    !articlePages[currentPage] ? currentPage.toString() : null,
+    fetcher,
+  );
 
   useEffect(() => {
-    (async () => {
-      if (articlePages[currentPage]) return;
+    if (!newResponse) return;
 
-      setFetching(true);
-      const response = await fetch(
-        "/api/utils/paginate/?" +
-          new URLSearchParams({
-            pageSize: pageSize.toString(),
-            cursor: currentCursor || "",
-          }).toString(),
-      );
-      const { data, newCursor } = await response.json();
-      setFetching(false);
-      setArticlePages((prev) => {
-        const result = [...prev];
-        result[currentPage] = data;
-        return result;
-      });
-      setCurrentCursor(newCursor);
-    })();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [currentPage]);
+    const { data, newCursor } = newResponse;
+    setArticlePages((prev) => {
+      if (prev[currentPage]) return prev;
+
+      const result = [...prev];
+      result[currentPage] = data;
+      return result;
+    });
+    setCurrentCursor(newCursor);
+  }, [newResponse, currentPage]);
 
   return {
     // Return last page if current page doesn't exist
     data: articlePages[currentPage] || articlePages[articlePages.length - 1],
-    fetching,
+    fetching: isLoading,
     onPageChange: setCurrentPage,
     currentPage,
   };
