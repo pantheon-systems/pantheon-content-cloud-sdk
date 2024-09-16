8b888a952090783809f01ad13383ad6c2cf88355
Kevin Stubbs <kevin@pubgenius.io>
2024-09-16 22:32:36 +0200

    Fix preamble mispelling.

--- packages/core/src/lib/gql.ts
+++ packages/core/src/lib/gql.ts
@@ -73,7 +73,7 @@ export function generateListArticlesGQL({
     $contentType: ContentType
     $publishingLevel: PublishingLevel
     $filter: ArticleFilterInput
-    $premable: String
+    $preamble: String
   ) {
     articlesv3(
       pageSize: $pageSize
