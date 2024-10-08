572fb9720bd5511386300af4d7ec4736853e534f
Kevin Stubbs <kevin@pubgenius.io>
2024-10-08 18:06:18 +0200

    Initial implementation of metadata groups in SDK + starter kit (1/3rd of them).

--- starters/nextjs-starter-ts/pages/api/pantheoncloud/[...command].ts
+++ starters/nextjs-starter-ts/pages/api/pantheoncloud/[...command].ts
@@ -4,11 +4,37 @@ import {
 } from "@pantheon-systems/pcc-react-sdk";
 import { serverSmartComponentMap } from "../../../components/smart-components";
 
+const authors = [
+  {
+    id: 1,
+    name: "James T. Kirk",
+    image: "https://en.wikipedia.org/wiki/File:William_Shatner_Star_Trek.JPG",
+  },
+  {
+    id: 2,
+    name: "Spock",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/f/fa/Leonard_Nimoy_as_Spock_1967.jpg/440px-Leonard_Nimoy_as_Spock_1967.jpg",
+  },
+];
+
 export const pantheonAPIOptions: PantheonAPIOptions = {
   resolvePath: (article) => `/articles/${article.slug || article.id}`,
   smartComponentMap: serverSmartComponentMap,
   componentPreviewPath: (componentName) =>
     `/component-preview/${componentName}`,
+  metadataGroups: [
+    {
+      label: "Author",
+      groupIdentifier: "AUTHOR",
+      schema: {
+        name: "string",
+        image: "image",
+      },
+      get: (id: string) => authors.find((x) => x.id?.toString() === id),
+      list: () => authors,
+    },
+  ],
 };
 
 export default PantheonAPI(pantheonAPIOptions);
