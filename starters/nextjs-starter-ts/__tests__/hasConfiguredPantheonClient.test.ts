f36000b0624200cf52558c35306d2ded2ceec58c
Renjith Warrier <renjithwarrier@pantheon.io>
2025-01-20 12:06:39 +0530

    Renjith | Update tests in nextjs-starter-ts

--- starters/nextjs-starter-ts/__tests__/hasConfiguredPantheonClient.test.ts
+++ starters/nextjs-starter-ts/__tests__/hasConfiguredPantheonClient.test.ts
@@ -1,17 +1,66 @@
-import { pantheonAPIOptions } from "../pages/api/pantheoncloud/[...command]";
+import { Site } from "@pantheon-systems/pcc-react-sdk/*";
+import { getPantheonAPIOptions } from "../pages/api/pantheoncloud/[...command]";
+
 
 describe("hasConfiguredPantheonClient", () => {
+  // Define a dummy site
+  const site: Site = {
+    id: "123",
+    name: "test",
+    url: "https://test.com",
+    domain: "test.com",
+    tags: [],
+    metadataFields: {},
+    contentStructure: {
+      active: [],
+    },
+  };
+
   it("Pantheon API options have been filled out", () => {
-    expect(pantheonAPIOptions.smartComponentMap.MEDIA_PREVIEW).toBeDefined();
+    expect(
+      getPantheonAPIOptions(site).smartComponentMap.MEDIA_PREVIEW,
+    ).toBeDefined();
   });
 
   it("Resolve document by article id", () => {
-    expect(pantheonAPIOptions.resolvePath({ id: "123" })).toBe("/articles/123");
+    expect(getPantheonAPIOptions(site).resolvePath({ id: "123" })).toBe(
+      "/articles/123",
+    );
   });
 
   it("Resolve document by article slug", () => {
     expect(
-      pantheonAPIOptions.resolvePath({ id: undefined, slug: "foo-bar-slug" }),
+      getPantheonAPIOptions(site).resolvePath({
+        id: "123",
+        slug: "foo-bar-slug",
+      }),
     ).toBe("/articles/foo-bar-slug");
+  });
+
+  it("Resolve document to the right content structure path", () => {
+    // Add content structure to the site
+    site.contentStructure = {
+      active: [
+        {
+          id: "category-1",
+          name: "foo",
+          type: "category",
+          children: [
+            {
+              id: "nested-category-1",
+              name: "bar",
+              type: "category",
+              children: [{ id: "123", name: "baz", type: "article" }],
+            },
+          ],
+        },
+      ],
+    };
+    expect(
+      getPantheonAPIOptions(site).resolvePath({
+        id: "123",
+        slug: "foo-bar-slug",
+      }),
+    ).toBe("/articles/foo/bar/foo-bar-slug");
   });
 });
